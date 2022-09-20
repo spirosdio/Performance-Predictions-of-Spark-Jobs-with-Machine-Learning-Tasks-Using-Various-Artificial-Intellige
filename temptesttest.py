408aec654495cd009d5dc7c31edf85431a204f7a
Spiros Diochnos <you@example.com>
2022-09-21 02:50:16 +0300

    some cleaning

--- temptesttest.py
+++
@@ -1,9 +0,0 @@
-array1 = [1, 99, 3, 4]
-
-
-array2 = [5, 6, 7, 8]
-
-# for everyelement in both arrays keep the max
-quortersHeap = [max(x, y) for x, y in zip(array1, array2)]
-
-print(quortersHeap)
